7e275e7e6ac595fe5f931754efd0244ebaf84fcf
maplehe7 <144498852+maplehe7@users.noreply.github.com>
2024-05-17 16:37:50 -0700

    ww

--- script.js
+++ script.js
@@ -3,12 +3,9 @@ email = document.getElementById("email")
 amount = document.getElementById("amount")
 
 function sendEmail(){
-    var link = "mailto:me@example.com"
-    + "?cc=myCCaddress@example.com"
-    + "&subject=" + encodeURIComponent("This is my subject")
-    + "&body=" + encodeURIComponent(document.getElementById("amount").value)
+   window.location.href = "mailto:maplehe7@gmail.com"
 ;
-window.location.href = link;
+
 
 
 }
